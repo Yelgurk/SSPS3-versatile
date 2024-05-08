8862b123f3cf05892322985306c9f6e8534bc664
Maxim <maksim.krugley@bk.ru>
2024-05-08 10:16:48 +0300

    application(main) solution prep

--- SSPS3_Master_Application/include/main.hpp
+++ SSPS3_Master_Application/include/main.hpp
@@ -11,11 +11,3 @@
 extern uint8_t FRAM_address;
 extern TwoWire * itcw;
 extern STM32_slave * STM32;
-
-FRAM_Var<boolean>       mem_Timer1(123, FRAM_ALLOC(boolean));
-FRAM_Var<uint8_t>       mem_Timer2(123, FRAM_ALLOC(uint8_t));
-FRAM_Var<uint16_t>      mem_Timer3(123, FRAM_ALLOC(uint16_t));
-FRAM_Var<uint32_t>      mem_Timer4(123, FRAM_ALLOC_ADDR(uint32_t, 100));
-FRAM_Var<uint32_t>      mem_Timer5(123, FRAM_ALLOC(uint32_t));
-
-uint8_t OptIn_state[8] = { 0 };
--- SSPS3_Master_Application/src/main.cpp
+++ SSPS3_Master_Application/src/main.cpp
@@ -1,12 +1,8 @@
 #include "../include/main.hpp"
 
-#define FRAM_DEMO_WRITE   1
-
 uint8_t FRAM_address = FRAM_I2C_ADDR;
 TwoWire * itcw;
 STM32_slave * STM32;
-
-void print(bool only_digital = false);
 
 void setup()
 {
@@ -19,157 +15,10 @@ void setup()
     itcw->begin(SDA, SCL, 400000);
 
     STM32 = new STM32_slave(STM_I2C_ADDR);
-
-    delay(5000);
-
-    if (true)
-    {
-        Serial.print(mem_Timer1.isInitialized());
-        Serial.print(" | ");
-        Serial.print(mem_Timer2.isInitialized());
-        Serial.print(" | ");
-        Serial.print(mem_Timer3.isInitialized());
-        Serial.print(" | ");
-        Serial.print(mem_Timer4.isInitialized());
-        Serial.print(" | ");
-        Serial.print(mem_Timer5.isInitialized());
-        Serial.println();
-        Serial.println();
-
-        if (!mem_Timer1.isInitialized()) mem_Timer1.unset();
-        if (!mem_Timer2.isInitialized()) mem_Timer2.unset();
-        if (!mem_Timer3.isInitialized()) mem_Timer3.unset();
-        if (!mem_Timer4.isInitialized()) mem_Timer4.unset();
-        if (!mem_Timer5.isInitialized()) mem_Timer5.unset();
-    }
-
-    if (true)
-    {
-        Serial.print(mem_Timer1);
-        Serial.print(" | ");
-        Serial.print(mem_Timer2);
-        Serial.print(" | ");
-        Serial.print(mem_Timer3);
-        Serial.print(" | ");
-        Serial.print(mem_Timer4);
-        Serial.print(" | ");
-        Serial.print(mem_Timer5);
-        Serial.println();
-        Serial.println();
-    }
-
-    if (true)
-    {
-        mem_Timer1.set(290);
-        mem_Timer2.set(290);
-        mem_Timer3.set(290 * 100);
-        mem_Timer4.set(290 * 100 * 2);
-        mem_Timer5.set(290 * 100 * 4);
-
-        Serial.print((boolean)290);
-        Serial.print(" | ");
-        Serial.print((uint8_t)290);
-        Serial.print(" | ");
-        Serial.print((uint16_t)(290 * 100));
-        Serial.print(" | ");
-        Serial.print((uint32_t)(290 * 100 * 2));
-        Serial.print(" | ");
-        Serial.print((uint32_t)(290 * 100 * 4));
-        Serial.println();
-        Serial.println();
-    }
-
-    if (true)
-    {
-        Serial.print(mem_Timer1);
-        Serial.print(" | ");
-        Serial.print(mem_Timer2);
-        Serial.print(" | ");
-        Serial.print(mem_Timer3);
-        Serial.print(" | ");
-        Serial.print(mem_Timer4);
-        Serial.print(" | ");
-        Serial.print(mem_Timer5);
-        Serial.println();
-        Serial.println();
-    }
-
-    if (true)
-    {
-        Serial.print(mem_Timer1.getAddress());
-        Serial.print(" | ");
-        Serial.print(mem_Timer2.getAddress());
-        Serial.print(" | ");
-        Serial.print(mem_Timer3.getAddress());
-        Serial.print(" | ");
-        Serial.print(mem_Timer4.getAddress());
-        Serial.print(" | ");
-        Serial.print(mem_Timer5.getAddress());
-        Serial.println();
-        Serial.println();
-    }
 }
 
-int pin = 0;
-uint32_t ms_old = 0,
-         ms_curr = 0,
-         ms_old_2 = 0,
-         ms_curr_2 = 0;
 
 void loop()
 {
-    if (interrupted_by_slave)
-    {
-        interrupted_by_slave = false;
-
-        print(true);
-
-        for (uint8_t i = 0; i < 8; i++)
-            STM32->set(COMM_SET::RELAY, i, OptIn_state[i]);
-    }
-
-
-    if ((ms_curr_2 = millis()) - ms_old_2 > 1000)
-    {
-        ms_old_2 = ms_curr_2;
-        print();
-    }
-}
-
-void print(bool only_digital)
-{
-    Serial.print(STM32->get_kb());
-
-    Serial.print(" | ");
-
-    Serial.print(OptIn_state[0] = STM32->get(COMM_GET::DGIN, 0));
-    Serial.print(" ");
-    Serial.print(OptIn_state[1] = STM32->get(COMM_GET::DGIN, 1));
-    Serial.print(" ");
-    Serial.print(OptIn_state[2] = STM32->get(COMM_GET::DGIN, 2));
-    Serial.print(" ");
-    Serial.print(OptIn_state[3] = STM32->get(COMM_GET::DGIN, 3));
-    Serial.print(" ");
-    Serial.print(OptIn_state[4] = STM32->get(COMM_GET::DGIN, 4));
-    Serial.print(" ");
-    Serial.print(OptIn_state[5] = STM32->get(COMM_GET::DGIN, 5));
-    Serial.print(" ");
-    Serial.print(OptIn_state[6] = STM32->get(COMM_GET::DGIN, 6));
-    Serial.print(" ");
-    Serial.print(OptIn_state[7] = STM32->get(COMM_GET::DGIN, 7));
     
-    if (!only_digital)
-    {
-        Serial.print(" | ");
-
-        Serial.print(STM32->get(COMM_GET::ANIN, 0));
-        Serial.print(" ");
-        Serial.print(STM32->get(COMM_GET::ANIN, 1));
-        Serial.print(" ");
-        Serial.print(STM32->get(COMM_GET::ANIN, 2));
-        Serial.print(" ");
-        Serial.println(STM32->get(COMM_GET::ANIN, 3));
-    }
-    else
-        Serial.println();
 }
