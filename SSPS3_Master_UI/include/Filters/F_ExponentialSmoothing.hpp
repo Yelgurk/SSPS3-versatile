1df5713de91b18d517765ce14128db209ec4064f
Maxim <maksim.krugley@bk.ru>
2024-07-08 09:27:03 +0300

    3 base filters for adc added

---
+++ SSPS3_Master_UI/include/Filters/F_ExponentialSmoothing.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#ifndef F_ExponentialSmoothing_hpp
+#define F_ExponentialSmoothing_hpp
+
+#include <Arduino.h>
+#include "Filter.hpp"
+
+class ExponentialFilter : public Filter
+{
+private:
+    float alpha;
+    float filtered_value;
+    bool initialized;
+
+public:
+    ExponentialFilter(float alpha) : alpha(alpha), filtered_value(0), initialized(false)
+    {}
+
+    void add_value(uint16_t value) override
+    {
+        if (!initialized)
+        {
+            filtered_value = value;
+            initialized = true;
+        }
+        else
+            filtered_value = alpha * value + (1 - alpha) * filtered_value;
+    }
+
+    uint16_t get_filtered_value() const override {
+        return static_cast<uint16_t>(filtered_value);
+    }
+};
+
+#endif
---
+++ SSPS3_Master_UI/include/Filters/FilterValue.hpp
@@ -0,0 +1,47 @@
+#pragma once
+#ifndef FilterValue_hpp
+#define FilterValue_hpp
+
+#include <Arduino.h>
+#include "Filter.hpp"
+
+class FilterValue
+{
+private:
+    Filter* current_filter;
+    uint16_t min_value;
+    uint16_t max_value;
+
+public:
+    FilterValue(Filter* filter, uint16_t min_value = 0, uint16_t max_value = 4095)
+        : current_filter(filter), min_value(min_value), max_value(max_value) {}
+
+    void set_filter(Filter* filter) {
+        current_filter = filter;
+    }
+
+    void set_min_max(uint16_t min_value, uint16_t max_value) {
+        this->min_value = min_value;
+        this->max_value = max_value;
+    }
+
+    uint16_t get_filtered_value() const {
+        return current_filter->get_filtered_value();
+    }
+
+    uint16_t get_filtered_value_relative() const {
+        uint16_t filtered = current_filter->get_filtered_value();
+        return static_cast<uint16_t>((filtered - min_value) * 4095 / (max_value - min_value));
+    }
+
+    double get_filtered_value_percentage() const {
+        uint16_t filtered = current_filter->get_filtered_value();
+        return static_cast<double>(filtered - min_value) * 100 / (max_value - min_value);
+    }
+
+    void add_value(uint16_t value) {
+        current_filter->add_value(value);
+    }
+};
+
+#endif
