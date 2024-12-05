1785b67850886c01e72964c439295d62728c78fb
Maxim <maksim.krugley@bk.ru>
2024-12-05 19:11:55 +0300

    new plc pcb design adaptation. part 3

--- SSPS3_Slave_STM32/include/main.hpp
+++ SSPS3_Slave_STM32/include/main.hpp
@@ -4,6 +4,9 @@
 #include <algorithm>
 #include "./I2C_Service.hpp"
 
+//#define SSPS3F1_V1
+#define SSPS3F1_BLACKOUT_EDITION
+
 /* Keypad 4x4 var's */
 #define KB_Row                      4
 #define KB_Col                      4
@@ -13,8 +16,26 @@
 #define HOLD_x                      1.2
 #define HOLD_min_ms                 50
 
-uint8_t KB_row_pin[KB_Row] = {PB11, PB13, PA9, PA11};
-uint8_t KB_col_pin[KB_Col] = {PC7, PC9, PB14, PC6};
+uint8_t KB_row_pin[KB_Row] =
+{
+#ifdef SSPS3F1_V1
+    PB11, PB13, PA9, PA11
+#endif
+#ifdef SSPS3F1_BLACKOUT_EDITION
+    PA12, PA11, PA10, PA9
+#endif
+};
+
+uint8_t KB_col_pin[KB_Col] =
+{
+#ifdef SSPS3F1_V1
+    PC7, PC9, PB14, PC6
+#endif
+#ifdef SSPS3F1_BLACKOUT_EDITION
+    PA8, PC9, PC8, PC7
+#endif
+};
+
 uint8_t KeyNew = KB_Await;
 uint8_t KeyPressed = KB_Await;
 uint16_t KeyHoldDelay = HOLD_begin_ms;
@@ -45,10 +66,17 @@ TwoWire * itcw;
 I2C_Service * I2C;
 
 /* I2C var's */
-#define SDA                     PB7
-#define SCL                     PB6
-#define INT                     PB8
 #define STM_I2C_ADR             0x30
+#ifdef SSPS3F1_V1
+    #define SDA                     PB7
+    #define SCL                     PB6
+    #define INT                     PB8
+#endif
+#ifdef SSPS3F1_BLACKOUT_EDITION
+    #define SDA                     PB11
+    #define SCL                     PB10
+    #define INT                     PA6
+#endif
 
 /* STM32 slave calc func for i2c data transmittion */
 #define ARR_SIZE(arr, type)         (sizeof(arr) / sizeof(type))
@@ -60,30 +88,58 @@ I2C_Service * I2C;
 String kbMsg = "";
 
 /* I/O defines and arr var's based on defines */
-#define STM_RELAY_1     PB1
-#define STM_RELAY_2     PB10
-#define STM_RELAY_3     PB12
-#define STM_RELAY_4     PB15
-#define STM_RELAY_5     PA12
-#define STM_RELAY_6     PA10
-#define STM_RELAY_7     PA8
-#define STM_RELAY_8     PC8
+#ifdef SSPS3F1_V1
+    #define STM_RELAY_1     PB1
+    #define STM_RELAY_2     PB10
+    #define STM_RELAY_3     PB12
+    #define STM_RELAY_4     PB15
+    #define STM_RELAY_5     PA12
+    #define STM_RELAY_6     PA10
+    #define STM_RELAY_7     PA8
+    #define STM_RELAY_8     PC8
 
-#define STM_DAC_1       PA4
+    #define STM_DAC_1       PA4
 
-#define STM_OPT_IN_1    PB0
-#define STM_OPT_IN_2    PC5
-#define STM_OPT_IN_3    PC4
-#define STM_OPT_IN_4    PA7
-#define STM_OPT_IN_5    PA6
-#define STM_OPT_IN_6    PA5
-#define STM_OPT_IN_7    PA3
-#define STM_OPT_IN_8    PA2
+    #define STM_OPT_IN_1    PB0
+    #define STM_OPT_IN_2    PC5
+    #define STM_OPT_IN_3    PC4
+    #define STM_OPT_IN_4    PA7
+    #define STM_OPT_IN_5    PA6
+    #define STM_OPT_IN_6    PA5
+    #define STM_OPT_IN_7    PA3
+    #define STM_OPT_IN_8    PA2
 
-#define STM_ADC_1       PC0
-#define STM_ADC_2       PC1
-#define STM_ADC_3       PC2
-#define STM_ADC_4       PC3
+    #define STM_ADC_1       PC0
+    #define STM_ADC_2       PC1
+    #define STM_ADC_3       PC2
+    #define STM_ADC_4       PC3
+#endif
+#ifdef SSPS3F1_BLACKOUT_EDITION
+    #define STM_RELAY_1     PC4
+    #define STM_RELAY_2     PC5
+    #define STM_RELAY_3     PB0
+    #define STM_RELAY_4     PB1
+    #define STM_RELAY_5     PB13
+    #define STM_RELAY_6     PB14
+    #define STM_RELAY_7     PB15
+    #define STM_RELAY_8     PC6
+
+    #define STM_DAC_1       PA4
+
+    #define STM_OPT_IN_1    PB5
+    #define STM_OPT_IN_2    PB4
+    #define STM_OPT_IN_3    PB3
+    #define STM_OPT_IN_4    PD2
+    #define STM_OPT_IN_5    PC12
+    #define STM_OPT_IN_6    PC11
+    #define STM_OPT_IN_7    PC10
+    #define STM_OPT_IN_8    PA15
+
+    #define STM_ADC_1       PC3
+    #define STM_ADC_2       PC2
+    #define STM_ADC_3       PC1
+    #define STM_ADC_4       PC0
+#endif
 
 uint8_t Relay[8]
 {
