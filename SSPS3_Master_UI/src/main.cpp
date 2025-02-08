c0a9a036c8d806a8b03952e26d039da70df55eec
Maxim <maksim.krugley@bk.ru>
2025-02-08 11:54:24 +0300

    testing new code on plc. part 2

--- SSPS3_Master_UI/src/main.cpp
+++ SSPS3_Master_UI/src/main.cpp
@@ -42,6 +42,21 @@ void setup_watchdogs();
 #include "../my_demo/Core/Memory/x_var_data_center.h"
 #include "../my_demo/Core/Memory/Device/mem_i2c_fm24gl64.h"
 
+void _display_10_x_10_eeprom()
+{
+    for (unsigned char col = 0; col < 10; col++)
+    {
+        for (unsigned char row = 0; row < 10; row++)
+        {
+            unsigned char _address = col * 10 + row;
+            char x = XStorageDispatcher->get_i_read_write()->byte_read(_address);
+            Serial.printf("%03d ", x);
+        }
+        Serial.println();
+    }
+    Serial.println();
+}
+
 /******************************************************************************************/
 
 void setup()
@@ -74,57 +89,24 @@ void setup()
 
     XStorageDispatcher->add_device(_ee_bank_1.get_i_read_write());
     XStorageDispatcher->add_device(_ee_bank_2.get_i_read_write());
-    //XStorage;
-
-    for (unsigned char col = 0; col < 10; col++)
-    {
-        for (unsigned char row = 0; row < 10; row++)
-        {
-            unsigned char _address = col * 10 + row;
-
-            char x = XStorageDispatcher->get_i_read_write()->byte_read(_address);
-            Serial.printf("%03d ", x);
-
-            //XStorageDispatcher->get_i_read_write()->write(_address, &_address, sizeof(_address));
-        }
-
-        Serial.println();
-    }
-    Serial.println();
 
     unsigned char lol = 123;
-    XStorageDispatcher->write(2, &lol, sizeof(lol));
 
-    for (unsigned char col = 0; col < 10; col++)
-    {
-        for (unsigned char row = 0; row < 10; row++)
-        {
-            unsigned char _address = col * 10 + row;
+    XStorage->TestVar1 = 15; // set 15 into var (both eeprom will save (uint16_t)15 + (uint8_t)crc)
+    _display_10_x_10_eeprom();
 
-            char x = XStorageDispatcher->get_i_read_write()->byte_read(_address);
-            Serial.printf("%03d ", x);
-        }
-
-        Serial.println();
-    }
-    Serial.println();
+    _ee_bank_1.write(2, &lol, sizeof(lol)); // broke crc for "TestVar1" on one of both equal records on both banks
+    _display_10_x_10_eeprom();
     
-    //XStorage->TestVar1 = 15;
-    XStorage;
+    XStorage->TestVar1.load_from_ext_mem(); // call "load from external mem" for reading + comparing records from both banks and pulling valid one into var (+ rewrite "error banks" with valid data)
+    _display_10_x_10_eeprom();
 
-    for (unsigned char col = 0; col < 10; col++)
-    {
-        for (unsigned char row = 0; row < 10; row++)
-        {
-            unsigned char _address = col * 10 + row;
+    _ee_bank_1.write(2, &lol, sizeof(lol)); // broke crc
+    _ee_bank_2.write(2, &lol, sizeof(lol)); // for both banks (p.s. for checking "set default value")
+    _display_10_x_10_eeprom();
 
-            char x = XStorageDispatcher->get_i_read_write()->byte_read(_address);
-            Serial.printf("%03d ", x);
-        }
-
-        Serial.println();
-    }
-    Serial.println();
+    XStorage->TestVar1.load_from_ext_mem(); // call "load from extrenal mem" and "set default value" will be called (p.s. default == value on init)
+    _display_10_x_10_eeprom();
 
     while(1)
     {}
